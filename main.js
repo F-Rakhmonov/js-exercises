4f33f3634ceca9c646e3887bc7b4f796e0b35a7f
fayz033 <f-rakhmonov@list.ru>
2023-11-08 23:46:18 +0500

    fayzullo

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-let message;
+// 1
+// let message;
+// if(a+b<4){
+//     message='Below'
+// }else{
+//     message='Over'
+// }
+
+// message = a + b < 4 ? (message = "Below") : (message = "Over");
+
+// 2
+// if(login=='Employee'){
+//     message="Hello"
+// }else if(login=='Director'){
+//     message="Greatings"
+// }else if(login==''){
+//     message='No login'
+// }else{
+//     message=''
+// }
+// let login = "Director";
+// message =
+//   login == "Employee"
+//     ? "Hello"
+//     : login == "Director"
+//     ? "Greatings"
+//     : login == ""
+//     ? "No login"
+//     : "";
+
+// console.log(message);
+
 
